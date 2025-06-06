2cf29a052d85da37abce51a680fef3ee5144faf1
Thisarani Jayaweera <thisarani.20232802@iit.ac.lk>
2025-06-06 08:53:11 +0530

    Add get recent events function to graph manager

--- backend/mythograph/graph/graph_manager.py
+++ backend/mythograph/graph/graph_manager.py
@@ -19,3 +19,23 @@ class GraphManger:
         """
         with self.driver.session() as session:
             session.run(query, agent_id=agent_id, desc=action_description)
+
+    def get_recent_events(self, agent_id=None, limit=5):
+        if agent_id:
+            query="""
+            MATCH (a:Character {agent_id: $agent_id})-[PERFORMED]->(e:Event)
+            RETURN e.description As description
+            ORDER BY e.timestamp DESC LIMIT $LIMIT
+            """
+            params = {"agent_id": agent_id, "limit": limit}
+        else:
+            query = """
+            MATCH (e:Event)
+            RETURN e.description AS description
+            ORDER BY e.timestamp DESC LIMIT $limit
+            """
+            params = {"limit": limit}
+
+        with self.driver.session() as session:
+            result = session.run(query, **params)
+            return [{"description": record["description"]} for record in result]
