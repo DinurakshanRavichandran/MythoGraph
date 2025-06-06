93d54138eedd7dd2802630eed261728e6a6c877c
Thisarani Jayaweera <thisarani.20232802@iit.ac.lk>
2025-06-06 08:46:30 +0530

    Add changes to graph manager file by using class implementation.

--- backend/mythograph/graph/graph_manager.py
+++ backend/mythograph/graph/graph_manager.py
@@ -4,20 +4,18 @@ NEO4J_URI ="bolt://localhost:7687"
 NEO4J_USER = "neo4j"
 NEO4J_PASSWORD = "hardworking123"
 
-driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_URI, NEO4J_PASSWORD))
+class GraphManger:
+    def __init__(self):
+        self.driver = GraphDatabase.driver(NEO4J_URI, auth=(NEO4J_USER, NEO4J_PASSWORD))
 
-def add_character_travel(character_name, location_name):
-    query = """ 
-    MERGE (c:character {name: $character})
-    MERGE (l:location {name: $location})
-    MERGE (c)-[TRAVELED_TO]->(l)
-    """
-    with driver.session() as session:
-        session.run(query, character=character_name, location=location_name)
-    print("f Added: {character_name} traveled to {location_name}")
+    def close(self):
+        self.driver.close()
 
-if __name__ == "__main__":
-    add_character_travel("Elandor", "Ashen Vale")
-    add_character_travel("Nyra", "Crimson Peaks")
-
-    driver.close()
+    def add_event(self, agent_id, action_description):
+        query = """
+        MATCH (a:Character {agent_id: $agent_id})
+        CREATE (e:Event {description: $desc, timestamp()})
+        CREATE (a)-[:PERFORMED] ->(e)
+        """
+        with self.driver.session() as session:
+            session.run(query, agent_id=agent_id, desc=action_description)
